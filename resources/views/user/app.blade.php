bdc3649ee770bd373de4f7fa7313e5bda2579a59
Alisson Campos <alisson.campos.dev@gmail.com>
2020-09-28 17:52:24 -0300

    app

--- resources/views/user/app.blade.php
+++ resources/views/user/app.blade.php
@@ -20,12 +20,11 @@
   </footer>
 
   <!-- Bootstrap core JavaScript -->
-<<<<<<< HEAD
-  <script src={{ asset('user/js/jquery/jquery.min.js')}}></script>
-  <script src={{ asset('user/js/bootstrap/bootstrap.min.js')}}></script>
+  <script src={{ secure_asset('user/js/jquery/jquery.min.js')}}></script>
+  <script src={{ secure_asset('user/js/bootstrap/bootstrap.min.js')}}></script>
 
   <!-- Custom scripts for this template -->
-  <script src={{ asset('user/js/clean-blog.min.js') }}></script>
+  <script src={{ secure_asset('user/js/clean-blog.min.js') }}></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.6/js/star-rating.min.js" type="text/javascript"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.6/themes/krajee-svg/theme.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-star-rating/4.0.6/js/locales/pt-BR.js"></script>
@@ -38,13 +37,6 @@
         language:'pt-BR'
     });
 </script>
-=======
-  <script src={{ secure_asset('user/js/jquery/jquery.min.js')}}></script>
-  <script src={{ secure_asset('user/js/bootstrap/bootstrap.bundle.min.js')}}></script>
-
-  <!-- Custom scripts for this template -->
-  <script src={{ secure_asset('user/js/clean-blog.min.js') }}></script>
->>>>>>> 2c81c7429babd8e3ec03e8e3d4be802084dd42be
   @section('footer')
     @show
 

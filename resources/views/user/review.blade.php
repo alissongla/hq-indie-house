1c905476abf93a4b4817527729c4fd93f056a80b
Alisson Campos <alisson.campos.dev@gmail.com>
2020-11-10 20:32:59 -0300

    Subtitle nas críticas

--- resources/views/user/review.blade.php
+++ resources/views/user/review.blade.php
@@ -7,11 +7,12 @@
             <h3 style="text-align: center">Últimas Critícas</h3>
             @foreach ($reviews as $critica)
                 <div class="row mb-5">
-                    <div class="col-6">
+                    <div class="col-3">
                         <img src={{ secure_asset(Storage::disk('public')->url($critica->image)) }} alt="" width="100%">
                     </div>
-                    <div class="col-6" style="vertical-align: middle">
+                    <div class="col-9" style="vertical-align: middle">
                     <h2><a href="{{ route('review-post', $critica->slug)}}">{{ $critica->title }}</a></h2>
+                    <p>{{$critica->subtitle}}</p>
                     <p><span class="meta">Postado por {{ $critica->author }}, {{ $critica->created_at->diffForHumans()}}</p>
                     </div>
 
